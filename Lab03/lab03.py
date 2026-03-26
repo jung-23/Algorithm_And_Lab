370ab1093cb8af3174ccad1ce76472e1378d124b
github jung_23 <github jung23@koreatech.ac.kr>
2026-03-27 00:37:28 +0900

    lab03

--- Lab03/lab03.py
+++ Lab03/lab03.py
@@ -11,16 +11,16 @@ class MSSP:
     
     def crossMax(self,A,low,mid,high):
         sm=0
-        left_sum=1000
-        right_sum =1000
+        left_sum=0
         for i in range(mid,low-1,-1):
             sm+=A[i]
-            left_sum = max(left_sum,sum)
+            left_sum = max(left_sum,sm)
             
         sm=0
-        for i in range(mid,high+1):
+        right_sum =0
+        for i in range(mid+1,high+1):
             sm+=A[i]
-            right_sum=max(right_sum,sum)
+            right_sum=max(right_sum,sm)
         return left_sum+right_sum
 
     def mssp_dc(self,A,low,high):
@@ -28,16 +28,50 @@ class MSSP:
             return A[low]
         else:
             mid = (low+high)//2
-            return max[self.mssp_dc(A,low,mid),
+            return max(self.mssp_dc(A,low,mid),
                        self.mssp_dc(A,mid+1,high),
-                       self.crossMax(A,low,mid,high)]
-
+                       self.crossMax(A,low,mid,high))
+        
+    def mssp_dp(self,n,A):
+        smax = A[0]
+        cmax = A[0]
+        for i in range(2,n):
+            cmax = max(cmax + A[i], A[i])
+            smax = max(smax, cmax)
+        return smax
 class LZSS:
     def LZSS_bf(self,A):
-        pass
+        n = len(A)
+        max_len = 0
+        for i in range(n):
+            curr_sum = 0
+            for j in range(i, n):
+                curr_sum += A[j]
+                if curr_sum == 0:
+                    max_len = max(max_len, j - i + 1)
+        return max_len
 
     def LZSS_dc(self,A,low,high):
-        pass
+        if low >high:
+            return 0
+        if(low==high):
+            if A[low]==0:
+                return 1
+            else:
+                return 0
+        mid = (low + high) // 2
+        left_len = self.LZSS_dc(A, low, mid)
+        right_len = self.LZSS_dc(A, mid + 1, high)
+        
+        maxcross = 0
+        for i in range(mid, low - 1, -1):
+            sum = 0
+            for j in range(i, high + 1):
+                sum += A[j]
+                if sum == 0:
+                    maxcross = max(maxcross,j-i+1)
+        return max(left_len, right_len, maxcross)
+
     def LZSS_Prefix(self,A):
         map={}
         sm=0
@@ -54,24 +88,19 @@ class LZSS:
 
 class LZSSM:
     def LZSSM_BF(self,M):
-        n,m = len(M),len(m[0])
+        n,m = len(M),len(M[0])
         marea =0
         for r1 in range(n):
             for c1 in range(m):
                 for r2 in range(r1,n):
                     for c2 in range(c1,m):
-
-
-
                         total =0
                         for i in range(r1,r2+1):
-                            for k in rnage(c1,c2+1):
-                                total +=M[i][j]
-
-                    
-                        if total ==0 :
-                            area = (r2-r1+1)*(c2-21+1)
-                            marea=max(marea,area)
+                            for k in range(c1,c2+1):
+                                total +=M[i][k]
+                            if total ==0 :
+                                area = (r2-r1)*(c2-c1)
+                                marea=max(marea,area)
         return marea
     
     def compute_prefix_matrix(self,M):
@@ -79,8 +108,49 @@ class LZSSM:
         p =[[0]*(m+1) for _ in range(n+1)]
         for i in range(n+1):
             for j in range(1, m+1):
-                p[i][j] = (M[i][j] + p[i-1][j] + p[i][j-1] - p[i-1][j-1])
+                p[i][j] = (M[i-1][j-1] + p[i-1][j] + p[i][j-1] - p[i-1][j-1])
         return p
     
     def LZSSM_Prefix(self,M):
-        pass
+        P=self.compute_prefix_matrix(M)
+        n,m = len(M),len(M[0])
+        marea =0
+        for r1 in range(n):
+            for c1 in range(m):
+                for r2 in range(r1,n):
+                    for c2 in range(c1,m):
+                        total = P[r2][c2] - P[r1-1][c2] - P[r2][c1-1] + P[r1-1][c1-1]
+                        if total ==0 :
+                            area = (r2-r1)*(c2-c1)
+                            marea=max(marea,area)
+        return marea
+
+    def LZSSM_RC(self, M):
+        if not M or not M[0]:
+            return 0
+        
+        n, m = len(M), len(M[0])
+        max_area = 0
+
+        for r1 in range(n):
+            column_sums = [0] * m
+            
+            for r2 in range(r1, n):
+                for c in range(m):
+                    column_sums[c] += M[r2][c]
+                
+                hash_map = {0: -1}
+                current_sum = 0
+                
+                for i in range(m):
+                    current_sum += column_sums[i]
+                    
+                    if current_sum in hash_map:
+                        length = i - hash_map[current_sum]
+                        area = (r2 - r1 + 1) * length
+                        if area > max_area:
+                            max_area = area
+                    else:
+                        hash_map[current_sum] = i
+
+        return max_area
--- Lab03/main.py
+++ Lab03/main.py
@@ -1,17 +1,35 @@
-from lab03 import MSSP
+from lab03 import MSSP,LZSS,LZSSM
 
 def tst_mssp():
     l3=MSSP()
     A= [2,-3,5,-9,6,7,-4,5,-2,1,-3,8]
+    print("\n>>> MSSP"+"-"*20)
     print("Brute force {}",format(l3.mssp_bf(A)))
-    print("DC {}",format(l3.mssp_dc(A)))
-    print("Dp {}",format(l3.mssp_bf(A)))
+    print("DC {}",format(l3.mssp_dc(A,0,len(A)-1)))
+    print("Dp {}",format(l3.mssp_dp(len(A),A)))
+
+def tst_LZSS():
+    l3=LZSS()
+    A= [2,-3,5,-9,6,7,-4,5,-2,1,-3,8]
+    print("\n>>> LZSS"+"-"*20)
+    print("Brute force {}",format(l3.LZSS_bf(A)))
+    print("DC {}",format(l3.LZSS_dc(A,0,len(A)-1)))
+    print("Prefix {}",format(l3.LZSS_Prefix(A)))
+
+def tst_LZSSM():
+    l3=LZSSM()
+    A = [[1,2,-3],[3,-3,3],[1,-1,0]]
+    print("\n>>> LZSSM"+"-"*20)
+    print("Brute force {}",format(l3.LZSSM_BF(A)))
+    print("Prefix {}",format(l3.LZSSM_Prefix(A)))
+    print("RC {}",format(l3.LZSSM_RC(A)))
+
 
 def main():
     tst_mssp()
-    pass
+    tst_LZSS()
+    tst_LZSSM()
     
     
-    
-if __name__ =="main":
+if __name__ == "__main__":
     main()
