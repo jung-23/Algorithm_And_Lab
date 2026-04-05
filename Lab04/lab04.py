e167973110b65e721ada89eb741d4c9b7af9168a
github jung_23 <github jung23@koreatech.ac.kr>
2026-04-05 22:39:48 +0900

    LAB04

--- Lab04/lab04.py
+++ Lab04/lab04.py
@@ -20,14 +20,14 @@ class inversion:
                 count+=(mid-i+1)
                 temp.append(A[j])
                 j+=1
-            while i<=mid:
-                temp.append(A[i])
-                i+=1
-            while j<=right:
-                temp.append(A[j])
-                j+=1
-            for k in range(len(temp)):
-                A[left+k] = temp[k]
+        while i<=mid:
+            temp.append(A[i])
+            i+=1
+        while j<=right:
+            temp.append(A[j])
+            j+=1
+        for k in range(len(temp)):
+            A[left+k] = temp[k]
         return count
 
     def inversion_dc(self,A,left,right):
@@ -40,7 +40,29 @@ class inversion:
         return count
         
     def inversion_st(self,A):
-        pass
+            
+        max_val = max(A) + 1
+        bit = [0]*(max_val+1)
+
+        def update(i):
+            while i <= max_val:
+                bit[i] += 1
+                i += i & -i
+
+        def query(i):
+            s = 0
+            while i > 0:
+                s += bit[i]
+                i -= i & -i
+            return s
+
+        count = 0
+
+        for i in range(len(A)-1, -1, -1):
+            count += query(A[i]-1)
+            update(A[i])
+
+        return count
 
 
 class sorting:
@@ -51,12 +73,12 @@ class sorting:
         for i in range(0,n):
             C[A[i]]+=1
 
-        for i in range(i,k):
+        for i in range(1,k):
             C[i] += C[i-1]
 
         i = n-1
         while i>=0:
-            B[C[A[i]-1]] = A[i]
+            B[C[A[i]]-1] = A[i]
             C[A[i]] -= 1
             i-=1
         return B
@@ -70,7 +92,7 @@ class sorting:
             index = A[i]//place
             C[index % 10]+=1
         
-        for i in range(i,10):
+        for i in range(1,10):
             C[i] += C[i-1]
         
         i = n-1
@@ -83,26 +105,48 @@ class sorting:
             A[i] = B[i]
 
         
-    
-
     def radix_sort(self,A):
         max0 = max(A)
         place = 1
         while max0//place > 0 :
             self.place_sort(A,place)
             place *= 10
+        return A
 
+    def heapify(self, A, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and A[left] > A[largest]:
+            largest = left
+
+        if right < n and A[right] > A[largest]:
+            largest = right
+
+        if largest != i:
+            A[i], A[largest] = A[largest], A[i]
+            self.heapify(A, n, largest)
 
     def heap_sort(self,A):
-        pass
+        n = len(A)
+
+        for i in range(n//2 - 1, -1, -1):
+            self.heapify(A, n, i)
+
+        for i in range(n-1, 0, -1):
+            A[0], A[i] = A[i], A[0]  
+            self.heapify(A, i, 0)
+
+        return A
 
 
-    def merge(A,S,left,mid,right):
+    def merge(self,A,S,left,mid,right):
         k=left
         i=left
         j = mid+1
-        while(i ==mid and j<=right):
-            if A[i]==A[j]:
+        while(i <=mid and j<=right):
+            if A[i]<=A[j]:
                 S[k] = A[i]
                 i +=1
                 k+=1
@@ -112,8 +156,11 @@ class sorting:
                 k+=1
             
         if i > mid:
-            S[k:k+right-j+1] = A[i:mid+1]
-
+            S[k:k+right-j+1] = A[j:right+1]
+        else:
+            S[k:k+mid-i+1] = A[i:mid+1]
+        
+        A[left:right+1] = S[left:right+1]
                 
     def merg_sort(self,A,S,left,right):
         if left < right :
@@ -121,11 +168,12 @@ class sorting:
             self.merg_sort(A,S,left,mid)
             self.merg_sort(A,S,mid+1,right)
             self.merge(A,S,left,mid,right)
+        return A
 
 
         
 
-    def partion(A,low,high):
+    def partion(self,A,low,high):
         pivot = A[low]
         j =low
         for i in range(low+1,high+1):
@@ -142,5 +190,55 @@ class sorting:
             pp = self.partion(A,low,high)
             self.quick_sort(A,low,pp-1)
             self.quick_sort(A,pp+1,high)
+        return A
 
 
+class merge_sort_3way:
+
+    def merge_3way(self, A, left, mid1, mid2, right):
+        i = left
+        j = mid1 + 1
+        k = mid2 + 1
+
+        temp = []
+
+        while i <= mid1 or j <= mid2 or k <= right:
+
+            values = []
+
+            if i <= mid1:
+                values.append((A[i], 'i'))
+            if j <= mid2:
+                values.append((A[j], 'j'))
+            if k <= right:
+                values.append((A[k], 'k'))
+
+            val, idx = min(values)
+            temp.append(val)
+
+            if idx == 'i':
+                i += 1
+            elif idx == 'j':
+                j += 1
+            else:
+                k += 1
+
+        for t in range(len(temp)):
+            A[left + t] = temp[t]
+
+
+    def merge_sort_3way(self, A, left, right):
+
+        if left < right:
+
+            third = (right - left) // 3
+
+            mid1 = left + third
+            mid2 = mid1 + third + 1
+
+            self.merge_sort_3way(A, left, mid1)
+            self.merge_sort_3way(A, mid1 + 1, mid2)
+            self.merge_sort_3way(A, mid2 + 1, right)
+
+            self.merge_3way(A, left, mid1, mid2, right)
+        return A
--- Lab04/main.py
+++ Lab04/main.py
@@ -1,33 +1,44 @@
-from lab04 import inversion, sorting
+from lab04 import inversion, sorting, merge_sort_3way
 
 def tst_inversion():
     inv = inversion()
     A = [11,2,31,4,5,6,17,8]
-    print("count BF : ",inv.inversion_bf(A))
-    print("count DC : ",inv.inversion_dc(A,0,len(A)-1))
+    
+    print("\n"+"% Inversion test >>>")
+    print("count BF : ",inv.inversion_bf(A.copy()))
+    print("count DC : ",inv.inversion_dc(A.copy(),0,len(A)-1))
+    print("count ST : ",inv.inversion_st(A.copy()))
 
 def tst_sorting():
     s = sorting()
-    A = [3,4,7,7,9,1,2,1]
-    A2 = [777,123,456,111,134]
-    A3 = [3,7,12,9,5,2,1]
-    print(A)
-    B = s.countion_sort(A,max(A)+1)
-    print(B)
+    arr = [11,2,31,4,5,6,17,8]
+    
+    print("% Sorting test >>>")
 
-    print(A2)
-    s.radix_sort(A2)
-    print(A2)
-    s.quick_sort(A2,0,len(A2)-1)
-    print(A2)
-    print(A3)
-    S= [0]*len(A3)
-    s.merg_sort(A3,)
+    print("Original array:", arr)
+    print("Counting Sort:", s.countion_sort(arr.copy(), max(arr) + 1))
 
+    print("Radix Sort:", s.radix_sort(arr.copy()))
+
+    print("Quick Sort:", s.quick_sort(arr.copy(), 0, len(arr) - 1))
+
+    print("Merge Sort:", s.merg_sort(arr.copy(), [0] * len(arr), 0, len(arr) - 1))
+
+    print("Heap Sort:", s.heap_sort(arr.copy()))
+
+def tst_merge_sort_3way():
+    m = merge_sort_3way()
+    arr = [11,2,31,4,5,6,17,8]
+
+    print("\n"+"% 3-Way Merge Sort test >>>")
+    print("Original array:", arr)
+    print("3-Way Merge Sort:", m.merge_sort_3way(arr.copy(), 0, len(arr) - 1))
 
 def main():
-    tst_inversion()
     tst_sorting()
+    tst_inversion()
+    tst_merge_sort_3way()
+    print()
 
 if __name__=="__main__":
     main()
