104aeff63dda398450ecc64f4aba53f62d61ce60
jeongha_N <jung23@koreatech.ac.kr>
2026-05-20 15:15:33 +0900

    lab09

--- LAB09/lab09.py
+++ LAB09/lab09.py
@@ -41,3 +41,108 @@ class NQueens:
             if board[i] == col or board[i]-i == col-row or board[i]+i == col+row :
                 return False
         return True
+    
+class SumSS:
+    def __init__(self,s,W):
+        self.nn=0
+        self.pn=1
+        
+        remainingTotal = sum(s)
+        runningWeight = 0
+        n = len(s)
+        subset=[]
+        index = 0
+        self.sumss(s,subset,index,runningWeight,n,W,remainingTotal)
+
+
+
+    def sumss(self,s,subset,index,runningWeight,n,W,remainingTotal):
+        self.nn += 1
+        if runningWeight == W :
+            print("subset =",subset,"sum of subset",sum(subset))
+            return
+        elif index == n:
+            return
+        elif runningWeight > W and remainingTotal< W:
+            return
+        else:
+            self.pn += 1
+            # if we do not pick the element (right subtree)
+            remainingTotal -= s[index]
+            self.sumss(s,subset,index+1,runningWeight,n,W,remainingTotal)
+
+            # if we pick the element (left subtree)
+            remainingTotal -= s[index]
+            runningWeight += s[index]
+            subset += [s[index]]
+            self.sumss(s,subset,index-1,runningWeight,n,W,remainingTotal)
+
+
+class Sdoku:
+    def __init__(self,g):
+        self.grid = g
+        self.non = 0
+
+    def print_grid(self):
+        for row in self.grid:
+            print(''.join(['{:2d}'.format(item) for item in row]))
+
+    def empthyCell(self):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[i])):
+                if self.grid[i][j] == 0 :
+                    return(i,j)
+        return None
+    def isPromissing(self,num,pos):
+        for i in range(len(self.grid)):
+            if self.grid[pos[0]][i] == num and pos[1] != i:
+                return False
+        for i in range(len(self.grid)):
+            if self.grid[i][pos[1]] == num and pos[0] != i:
+                return False
+            
+        box_x = pos[1]//3
+        box_y = pos[0]//3
+        for i in range(box_y*3, box_y *3+3):
+            for j in range(box_x*3,box_x *3 +3):
+                if self.grid[i][j] == num and (i,j) != pos:
+                    return False
+        return True
+    
+    def solve(self):
+        # self.tn += 1
+        find = self.empthyCell()
+        if not find :
+            return True
+        else:
+            row, col = find
+
+        for i in range( 1, 10 ):
+            if self.isPromissing(i,(row,col)):
+                self.non += 1
+                self.grid[row][col] = i
+                # print(f"\nTrying {i} at ( {row}, {col}) - promising nodes : {self.non}")
+                # self.print_grid(self.grid)
+                # time.sleep(0.1) # slow down for visualization
+
+                if self.solve():
+                    return True
+                
+                #Backtrack
+                # print(f"Backtracking from( {row}, {col})"")
+
+                self.grid[row][col]=0
+
+                # self.print_grid(self.grid)
+                # time.sleep(0.1)
+        return False
+    
+    def run(self):
+        print("Start State:")
+        self.print_grid()
+        if self.solve():
+            print("Goal State: ")
+            self.print_grid()
+            print("Number of promising nodes:", self.non)
+        else:
+            print("No solution exists")
--- LAB09/main.py
+++ LAB09/main.py
@@ -1,4 +1,4 @@
-from lab09 import permutaltions,NQueens
+from lab09 import permutaltions,NQueens,SumSS,Sdoku
 
 def tstPermutation():
     st = "ABC"
@@ -8,11 +8,31 @@ def tstPermutation():
 def tstnQueens():
     nq=NQueens(10)
 
+def tstsumss():
+    S = [4,3,6,1,6]
+    W = 13
 
+    ss = SumSS(S,W)
+    print("Total nodes", ss.nn, " promissing nodes : ", ss.pn)
+
+def tstSudoku():
+    g = [[3,0,6,5,0,8,4,0,0],
+         [5,2,0,0,0,0,0,0,0],
+         [0,8,7,0,0,0,0,3,1],
+         [0,0,3,0,1,0,0,8,0],
+         [9,0,0,8,0,3,0,0,5],
+         [0,5,0,0,9,0,6,0,0],
+         [1,3,0,0,0,0,2,5,0],
+         [0,9,0,0,0,0,0,7,4],
+         [0,0,5,2,0,6,3,0,0]]
+    
+    sk = Sdoku(g)
+    sk.run()
 
 def main():
-    tstPermutation()
-    tstnQueens()
+    tstSudoku()
+
+
 
 
 
